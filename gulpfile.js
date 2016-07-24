ab1e2bb650a2b672fa1dfdd081c1f52a8e4a9991
Kushal Joshi <kushal@joshi.com>
2016-07-24 21:55:09 +0100

    First commit, basic psaadmin project with working linter. All latest versions and written in ES6

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+"use strict"
+
+const gulp = require("gulp")
+const connect = require("gulp-connect") // runs a web server
+const open = require("gulp-open") // opens a url
+const browserify = require("browserify") // Bundler
+const reactify = require("reactify") // transform JSX to JS
+const source = require("vinyl-source-stream") // text streams with gulp
+const concat = require("gulp-concat") // concatenates files
+const lint = require("gulp-eslint") // Lint JS, including JSX
+
+const config = {
+    port: 8001,
+    devBaseURL: "http://localhost",
+    paths: {
+        html: "./src/*.html",
+        js: "./src/**/*.js",
+        css: [
+            "node_modules/bootstrap/dist/css/bootstrap.min.css",
+            "node_modules/bootstrap/dist/css/bootstrap-theme.min.css"
+        ],
+        dist: "./dist",
+        mainJs: "./src/main.js"
+    }
+}
+
+//Start a local development server
+gulp.task("connect", () => {
+    connect.server({
+        root: ["dist"],
+        port: config.port,
+        base: config.devBaseURL,
+        livereload: true
+    })
+})
+
+gulp.task("open", ["connect"], () => {
+    gulp.src("dist/index.html")
+        .pipe(open({ uri: config.devBaseURL + ":" + config.port + "/" }))
+})
+
+gulp.task("html", () => {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload())
+})
+
+gulp.task("js", () => {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on("error", console.error.bind(console))
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest(config.paths.dist + "/scripts"))
+        .pipe(connect.reload())
+})
+
+gulp.task ("css", () => {
+    gulp.src(config.paths.css)
+        .pipe(concat("bundle.css"))
+        .pipe(gulp.dest(config.paths.dist + "/css"))
+})
+
+gulp.task("lint", () => {
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: "eslint.config.json"}))
+        .pipe(lint.format())
+        .pipe(eslint.failAfterError())
+})
+
+gulp.task("watch", () => {
+    gulp.watch(config.paths.html, ["html"])
+    gulp.watch(config.paths.js, ["js", "lint"])
+    gulp.watch(config.paths.css, ["css"])
+})
+
+gulp.task("default", ["html", "js", "css", "lint", "open", "watch"])
---
+++ src/main.js
@@ -0,0 +1,7 @@
+$ = jQuery = require("jquery")
+
+// test = 1 // Global variable test
+
+const App = console.log("Hello world from Browserify")
+
+module.exports = App
